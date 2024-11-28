e9484678131b36a3cb3094a869b0a179a8fb1f43
daria <gubskaya.dd@gmail.com>
2024-11-28 10:47:24 +0600

    Configure routing with React Router for PokemonsList and PokemonPage

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import PokemonsList from "./pages/PokemonsList";
+import PokemonPage from "./pages/PokemonPage";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<PokemonsList />} />
+                <Route path="/pokemon/:id" element={<PokemonPage />} />
+            </Routes>
+        </Router>
+    </StrictMode>
+);
